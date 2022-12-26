623f742ddcc279391657e3840db7173d6d98d0e8
Daniel Barton <mineminecraftpe687@gmail.com>
2022-12-26 13:34:03 +1100

    Push from Github Desktop

--- content.js
+++ content.js
@@ -1,24 +1,38 @@
 // Author: @Daniel4-scratch
 
-var pageHash = window.location.hash;
-pageHash = pageHash.replace("#", "");
-
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
 function login(token) {
-    setInterval(() => {
-        document.body.appendChild(document.createElement`iframe`).contentWindow.localStorage.token = `"${token}"`
-    }, 50);
-    setTimeout(() => {
-        location.reload();
-    }, 2500);
+    setCookie("token", token, 9999);
 }
 
-if (pageHash == "tokenlogin"){
+if (window.location.href == "https://www.postlit.dev/tokenlogin"){
     try{
     var token = prompt("Enter your token");
     login(token);
-    window.location.href = "https://discord.com/app";
+    window.location.href = "https://www.postlit.dev";
     }catch(err){
         alert("An error occured during the login process. Please try again.");
-        console.alert("Discord Token Login Error: " + err);
+        console.alert("PostLit Token Login Error: " + err);
     }
+}
+if (window.location.href == "https://www.postlit.dev/mytoken"){
+    document.body.innerHTML = '<div class="navbar"><a href="https://www.postlit.dev/"class="main-title-logo">postLit</a></div><h1>MyToken</h1><textarea readonly>'+getCookie('token')+'</textarea>'
 }
